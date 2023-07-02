6a2a8119572d447fc8a6e1dec29e59bb6d7709e9
damao <xiaoyuguan@hotmail.com>
2023-07-03 00:40:21 +0800

    :mute: suppress warnings

--- summarizer/suppress_warning.py
+++ summarizer/suppress_warning.py
@@ -3,3 +3,4 @@ import warnings
 
 warnings.simplefilter('ignore', category=NumbaDeprecationWarning)
 warnings.simplefilter('ignore', category=NumbaPendingDeprecationWarning)
+warnings.simplefilter('ignore', category=UserWarning)
